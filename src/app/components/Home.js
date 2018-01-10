c17cf7bfd556b7535d8d74e2d978d7e913f7f61e
Nelabh Kotiya <nelabh.kotiya@indiamart.com>
2018-01-10 17:18:55 +0530

    ReactJs Props

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,11 +1,31 @@
 import React from "react";
+import PropTypes from 'prop-types';
 export class Home extends React.Component{
 	render(){
+    let variable = "This is dynamic";
+    console.log(this.props);
 		return (
 			<div>
 				<p> New Home</p>
-				{2+3}
+        <p>Name : {this.props.name}</p>
+        <p>age : {this.props.age}</p>
+        <p>User's Name : {this.props.user.name}</p>
+        {2+3}
+        {variable }
+        <div>
+          <ul>
+            {this.props.user.hobbies.map((hobby,i)=><li key = {i}>{hobby}</li>)}
+          </ul>
+        </div>
+        <hr/>
+        {this.props.children}
 			</div>
 			);
 	}
 }
+Home.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  user: PropTypes.object,
+  children: PropTypes.element.isRequired
+};
--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,8 @@ import {Home} from "./components/Home";
 
 class App extends React.Component{
 	render(){
+    var name = "Nelabh",age =23, user = {name:"nel",hobbies:["singing","dancing"]};
+
 		return(
 			<div className = "container">
 				<div className = "row">
@@ -14,7 +16,9 @@ class App extends React.Component{
 				</div>
 				<div className = "row">
 					<div className = "col-xs-10 col-xs-offset-1">
-						<Home/>
+            <Home name = {name} age = {age} user = {user}>
+              <p>Children Text</p>
+            </Home>
 					</div>
 				</div>
 			</div>
