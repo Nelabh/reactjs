2b2142a21a3748e1afb6f4b46d628ef5abdb92bf
Nelabh Kotiya <nelabh.kotiya@indiamart.com>
2018-01-11 11:07:21 +0530

    ReactJs State of Components

--- src/app/components/Home2.js
+++ src/app/components/Home2.js
@@ -4,6 +4,10 @@ export class Home2 extends React.Component{
   constructor(props){
     super();
     this.age = props.age;
+    this.state = {
+      age: this.age,
+      status:0
+    };
   }
   onMakeOlder(){
     this.age += 3; 
@@ -13,6 +17,16 @@ export class Home2 extends React.Component{
     this.age -= 3; 
     console.log(this.age);
   }
+  onMakeOlderState(){
+    this.setState({
+      age:this.state.age + 3
+    });
+  }
+    onMakeYoungerState(){
+    this.setState({
+      age:this.state.age - 3
+    });
+  }
 	render(){
     let variable = "This is dynamic";
     console.log(this.props);
@@ -21,8 +35,17 @@ export class Home2 extends React.Component{
 				<p> New Home2</p>
         <p>Name : {this.props.name}</p>
         <p>age : {this.age}</p>
+        <p>State's age : {this.state.age}</p>
         <button onClick = {this.onMakeOlder.bind(this)} className = "btn btn-primary"> Make Me Older</button>
+        <br/>
+        <br/>
         <button onClick = {()=>this.onMakeYounger()} className = "btn btn-primary"> Make Me Younger</button>
+        <br/>
+        <br/>
+        <button onClick = {this.onMakeOlderState.bind(this)} className = "btn btn-primary"> Make Me Older using state</button>
+        <br/>
+        <br/>
+        <button onClick = {()=>this.onMakeYoungerState()} className = "btn btn-primary"> Make Me Younger using state</button>
 			</div>
 			);
 	}
