9b29238fdd7d6c4f40ba1612503a98093fbe3da0
Nelabh Kotiya <nelabh.kotiya@indiamart.com>
2018-01-11 11:27:23 +0530

    ReactJs Stateless Components

--- src/app/index.js
+++ src/app/index.js
@@ -3,6 +3,7 @@ import {render} from "react-dom";
 import {Header} from "./components/Header";
 import {Home} from "./components/Home";
 import {Home2} from "./components/Home2";
+import {Stateless} from "./components/Stateless";
 
 class App extends React.Component{
   
@@ -17,6 +18,12 @@ class App extends React.Component{
 					</div>
 				</div>
 				<div className = "row">
+          <div className = "col-xs-10 col-xs-offset-1">
+              <Stateless homeLink = "Stateless Home"/>
+          </div>
+        </div>
+      
+        <div className = "row">
 					<div className = "col-xs-10 col-xs-offset-1">
             <Home name = {name} age = {age} user = {user}>
               <p>Children Text</p>
